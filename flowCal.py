094ded914671572a69832dd44c6eec4161cc0fd0
Ruochun <ruochunz@gmail.com>
2022-05-03 13:22:46 -0500

    Use data based on silo tests

--- flowCal.py
+++ flowCal.py
@@ -18,20 +18,26 @@ def main():
     y = flow_data['data'][-1,:]
 
     # Now normalize data
-    upper_c, lower_c = 100., 0.
-    upper_r, lower_r = 0.5, 0.
+    upper_d, lower_d = 1., 0.
     upper_rho, lower_rho = 10., 0.
+    upper_D, lower_D = 5., 0.
     upper_y, lower_y = 1e5, 0.
-    c = (x[0,:] - lower_c) / (upper_c - lower_c)
-    r = (x[1,:] - lower_r) / (upper_r - lower_r)
+    """
+    D = (2.*x[0,:] - lower_D) / (upper_D - lower_D)
+    d = (2.*x[1,:] - lower_d) / (upper_d - lower_d)
     rho = (x[2,:] - lower_rho) / (upper_rho - lower_rho)
     #y = (y - np.min(y)) / (np.max(y) - np.min(y))
     #y = np.log(y)
     y = pred.normalize(y, upper=upper_y, lower=lower_y)
+    """
+    D = 2.*x[0,:]
+    d = 2.*x[1,:]
+    rho = x[2,:]
+    sqrt_g = np.sqrt(981.)
 
-    pred_r = np.array([0.07, 0.125,0.3])
+    pred_d = np.array([0.07, 0.125,0.3])
     pred_rho = np.array([0.5, 3.25,7.])
-    pred_r = (pred_r - lower_r) / (upper_r - lower_r)
+    pred_d = (pred_d - lower_d) / (upper_d - lower_d)
     pred_rho = (pred_rho - lower_rho) / (upper_rho - lower_rho)
 
     print(f"Running on PyMC3 v{pm.__version__}") 
@@ -45,15 +51,14 @@ def main():
         #p2 = pm.Uniform('p2', lower=-3, upper=3)
         #b = pm.Uniform('b', lower=-100, upper=100)
         
-        C = pm.Uniform('C', lower=0., upper=1000.)
-        k = pm.Uniform('k', lower=-10., upper=10.)
-        #p = pm.Uniform('p', lower=.5, upper=4.)
-        D = pm.Uniform('D', lower=0., upper=50.)
-        p = 5./2.
+        C = pm.Uniform('C', lower=0., upper=1e2)
+        k = pm.Uniform('k', lower=0., upper=5.)
+        p = pm.Uniform('p', lower=.5, upper=4.)
+        #p = 5./2.
         #D = 4.5
         sigma = pm.HalfNormal("sigma", sigma=1)
 
-        flow_rate = C * rho * (D - k*r)**p
+        flow_rate = C * sqrt_g * rho * (D - k*d)**p
         #flow_rate = np.log(C) + np.log(rho) + p*np.log(D - k*r)
         y_obs = pm.Normal("y_obs", mu=flow_rate, sigma=sigma, observed=y)
 
@@ -61,13 +66,13 @@ def main():
     # print(map_est)
 
     with basic_model:
-        step = pm.Metropolis(vars=[C, k, D, sigma])
-        #step = pm.Metropolis(vars=[C,k, p,sigma])
+        #step = pm.Metropolis(vars=[C, k, sigma])
+        step = pm.Metropolis(vars=[C,k, p,sigma])
         trace = pm.sample(nsteps, tune=nsteps//2, step=step, discard_tuned_samples=True, return_inferencedata=False,
-                #start={'C':0.2, 'k':1., 'p':2.5, 'sigma':0.1})
-                start={'C':0.2, 'k':1.,'D':3.,'sigma':0.1})
+                start={'C':10., 'k':1., 'p':1., 'sigma':0.1})
+                #start={'C':10., 'k':1.,'sigma':0.1})
         fig1 = az.plot_trace(trace)
-        fig2 = az.plot_autocorr(trace, var_names=['C', 'k', 'D'], max_lag=nsteps, combined=True)
+        fig2 = az.plot_autocorr(trace, var_names=['C', 'k', 'p'], max_lag=nsteps, combined=True)
         fig1.ravel()[0].figure.savefig("fig1.png")
         fig2.ravel()[0].figure.savefig("fig2.png")
 
--- pred.py
+++ pred.py
@@ -4,11 +4,8 @@ import numpy as np
 import scipy.io as sio
 import ctypes
 from numpy.ctypeslib import ndpointer
-import pymc3 as pm
-import arviz as az
-
-#p = 5./2.
-D = 4.5
+#import pymc3 as pm
+#import arviz as az
 
 def main():
     # Load data for VHB 4910
@@ -20,32 +17,43 @@ def main():
     y = flow_data['data'][-1,:]
     #y = np.log(y)
     # Now normalize data
+    sqrt_g = np.sqrt(981.)    
+    """
     upper_c, lower_c = 100., 0.
     upper_r, lower_r = 0.5, 0.
     upper_rho, lower_rho = 10., 0.
     upper_y, lower_y = 1e5, 0.
+    """
 
-    C = 0.000825
-    k = 1.775
-    p = 5./2. #1.50
-    pred_r = np.array([0.07, 0.125,0.3])
-    pred_rho = np.array([0.5, 3.25,7.])
-    pred_r = (pred_r - lower_r) / (upper_r - lower_r)
-    pred_rho = (pred_rho - lower_rho) / (upper_rho - lower_rho)
+    C = 0.16
+    k = 2.07
+    p = 2.59 
+    pred_d = 2.*np.array([0.04])
+    pred_rho = np.array([1.])
+    pred_D = 2.*np.array([2.2])
+    #pred_r = (pred_r - lower_r) / (upper_r - lower_r)
+    #pred_rho = (pred_rho - lower_rho) / (upper_rho - lower_rho)
 
     pred_y = []
-    out_r = []
+    out_d = []
+    out_D = []
     out_rho = []
-    for a_r in pred_r:
+    for a_d in pred_d:
         for a_rho in pred_rho:
-            out_r.append(a_r)
-            out_rho.append(a_rho)
-            pred_y.append(C * a_rho * (D - k*a_r)**p)
-            #pred_y.append(np.log(C) + np.log(a_rho) + p*np.log(D - k*a_r))
+            for a_D in pred_D:
+                out_d.append(a_d)
+                out_rho.append(a_rho)
+                out_D.append(a_D)
+                pred_y.append(C * a_rho * sqrt_g * (a_D - k*a_d)**p)
+    """
     print_arr(denormalize(out_r, lower=lower_r, upper=upper_r), "radius:")
     print_arr(denormalize(out_rho, lower=lower_rho, upper=upper_rho),'density:')
-    #print_arr(np.exp(denormalize(pred_y, lower=np.min(y), upper=np.max(y))),"flow rate pred:")
     print_arr(denormalize(pred_y,lower=lower_y, upper=upper_y),"flow rate pred:")
+    """
+    print_arr(out_d, "Particle diameter:")
+    print_arr(out_rho, "Density:")
+    print_arr(out_D, "Orifice diameter:")
+    print_arr(pred_y, "Predicted flowrate:")
 
 def print_arr(arr, arr_name="Array:"):
     print(arr_name)
